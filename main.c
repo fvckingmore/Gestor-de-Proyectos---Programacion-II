c3a4a143a51d29f8e1bdd9f92ada0692a494f589
Adrian Moreno <adrianemoreno.25@gmail.com>
2019-04-16 03:31:16 -0400

    Implementada parcialmente estrctura como parametro
    Se ha implementado estructura como parametro para mejorar el manejo de los datos
    del usuario que se ha logueado, sin tener que pasar individialmente lo campos como
    vairables independientes
    
    Cambios a ser confirmados:
    	modificado:     lib_user.c
    	modificado:     main.c

--- main.c
+++ main.c
@@ -9,8 +9,7 @@ int main() {
 
 	int op, v;	
 
-	char aux_username[15];	//MUESTRA EL NOMBRE DE USUARIO AL INGRESAR
-	int ID;	//GUARDA EL ID DEL ADMIN PARA ADMINISTRAR USUARIOS 
+	USER user;
 
 
 	
@@ -39,17 +38,17 @@ int main() {
 
 			} else if ( op == 1 ) {
 
-				switch ( f_login(aux_username, &ID) ) {
+				switch ( f_login(&user) ) {
 
 					case 1: 
 
-						f_admin_menu(aux_username, ID);
+						f_admin_menu(user);
 
 						break;
 
 					case 2:
 
-						f_standar_menu(aux_username);
+						f_standar_menu(user);
 
 						break;
 
